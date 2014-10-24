37cf3d1c769d5b7f95f5070df5d0cdec4cbc4f87
加藤 史也 <FumKATO@katou-no-MacBook-Air.local>
2014-10-24 17:09:35 +0900

    Delete code for experiment

--- client/Views/cnavi_tutorial_view.js
+++ client/Views/cnavi_tutorial_view.js
@@ -2,48 +2,15 @@ CNaviTutorialView = function(){
 	var _this = CNaviTutorialView;
 	
 	_this.prototype.show_alert = function(){
-		switch(Session.get('mode')){
-			case 'morning':
-				alert('Good Morning!');
-				break;
-			case 'night':
-				alert('Good Night!');
-				break;
-			default:
-				alert('Hello World!');
-				break;
-		}
+		//TODO
 	};
 	
 	_this.prototype.change_color = function(){
-		switch(Session.get('mode')){
-			case 'morning':
-				$('body').css('background', '#DDDDFF');
-				$('body').css('color', '#000077');
-				break;
-			case 'night':
-				$('body').css('background', '#000');
-				$('body').css('color', '#FFF');
-				break;
-			default:
-				$('body').css('background', '#FFF');
-				$('body').css('color', '#000');
-				break;
-		}
+		//TODO
 	};
 	
 	_this.prototype.render_topic = function(){
-		switch(Session.get('mode')){
-			case 'morning':
-				$('#hello').html('Morning Mode');
-				break;
-			case 'night':
-				$('#hello').html('Night Mode');
-				break;
-			default:
-				$('#hello').html('Default Mode');
-				break;
-		}
+		//TODO
 	};
 };
 
